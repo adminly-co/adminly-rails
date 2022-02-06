675e0cf43fb7d3a5a9b578ccf97cc0ac3b850169
Rami Bitar <rbitar@gmail.com>
2022-02-06 17:05:43 -0500

    Update routes to use /adminly

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: redirect { 
-    Rails.application.routes.url_helpers.dash_api_path 
+    Rails.application.routes.url_helpers.adminly_path 
   }
 
-  mount DashApi::Engine, at: "/api"
+  mount Adminly::Engine, at: "/adminly"
 
 end
