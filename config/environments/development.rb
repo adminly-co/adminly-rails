9656aaa58cbe61689c72a8068886de6005e03199
Rami Bitar <rbitar@gmail.com>
2022-03-08 18:09:45 -0500

    Belongs_to not required by default

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-  config.active_record.belongs_to_required_by_default = true
+  config.active_record.belongs_to_required_by_default = false
   
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.active_record.belongs_to_required_by_default = true
+  config.active_record.belongs_to_required_by_default = false
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
